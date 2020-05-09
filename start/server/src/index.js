2a43cace6edb098d9fb38af85b52cee5e346e482
WinFrid61 <38008327+WinFrid61@users.noreply.github.com>
2020-05-09 18:22:20 +0300

    Исправление ошибок

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,5 +1,4 @@
 const { ApolloServer } = require('apollo-server');
-const isEmail = require('isemail');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils');
 
@@ -10,15 +9,10 @@ const UserAPI = require('./datasources/user');
 
 const store = createStore();
 
-const server = new ApolloServer({
-  typeDefs,
+const isEmail = require('isemail');
 
-  resolvers,
-  dataSources: () => ({
-    launchAPI: new LaunchAPI(),
-    userAPI: new UserAPI({ store })
-  })
-  context: async ({ req }) => {
+const server = new ApolloServer({
+	context: async ({ req }) => {
     // simple auth check on every request
     const auth = req.headers && req.headers.authorization || '';
     const email = Buffer.from(auth, 'base64').toString('ascii');
@@ -30,6 +24,14 @@ const server = new ApolloServer({
     const user = users && users[0] || null;
 
     return { user: { ...user.dataValues } };
+  },
+  typeDefs,
+
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
 });
 
 server.listen().then(({ url }) => {
