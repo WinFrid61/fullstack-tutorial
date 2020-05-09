c4c7c2677d2ab2c360c7902d610925a619bda9ef
WinFrid61 <38008327+WinFrid61@users.noreply.github.com>
2020-05-09 17:54:46 +0300

    Добавим резолвер Mission

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -28,4 +28,12 @@ module.exports = {
      me: async (_, __, { dataSources }) =>
       dataSources.userAPI.findOrCreateUser(),
   }
+  Mission: {
+  // make sure the default size is 'large' in case user doesn't specify
+  missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+    return size === 'SMALL'
+      ? mission.missionPatchSmall
+      : mission.missionPatchLarge;
+  },
+},
 };
