a8f648f2c17bc2a97d4eaa5f042e8fa0158bbb22
WinFrid61 <38008327+WinFrid61@users.noreply.github.com>
2020-05-14 22:40:15 +0300

    Добавление виртуальных полей в данные сервера

--- start/client/src/resolvers.tsx
+++ start/client/src/resolvers.tsx
@@ -3,6 +3,14 @@ import { ApolloCache } from 'apollo-cache';
 import * as GetCartItemTypes from './pages/__generated__/GetCartItems';
 import * as LaunchTileTypes from './pages/__generated__/LaunchTile';
 import { Resolvers } from 'apollo-client'
+import gql from 'graphql-tag';
+import { GET_CART_ITEMS } from './pages/cart';
+
+export const schema = gql`
+  extend type Launch {
+    isInCart: Boolean!
+  }
+`;
 
 export const typeDefs = gql`
   extend type Query {
@@ -30,7 +38,19 @@ interface ResolverMap {
 }
 
 interface AppResolvers extends Resolvers {
-  // We will update this with our app's resolvers later
+  Launch: ResolverMap;
 }
 
-export const resolvers = {};
+export const resolvers: AppResolvers = {
+  Launch: {
+    isInCart: (launch: LaunchTileTypes.LaunchTile, _, { cache }): boolean => {
+      const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({ 
+        query: GET_CART_ITEMS 
+      });
+      if (queryResult) {
+        return queryResult.cartItems.includes(launch.id)
+      } 
+      return false;
+    }
+  },
+};
