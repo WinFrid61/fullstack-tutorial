271472c134fb98b5a1b5d377816fbfe91e8ef7b7
WinFrid61 <38008327+WinFrid61@users.noreply.github.com>
2020-05-14 23:07:59 +0300

    Локальные резолверы (+Исправления)

--- start/client/src/resolvers.tsx
+++ start/client/src/resolvers.tsx
@@ -1,4 +1,4 @@
-export default {}import gql from 'graphql-tag';
+import gql from 'graphql-tag';
 import { ApolloCache } from 'apollo-cache';
 import * as GetCartItemTypes from './pages/__generated__/GetCartItems';
 import * as LaunchTileTypes from './pages/__generated__/LaunchTile';
@@ -39,18 +39,27 @@ interface ResolverMap {
 
 interface AppResolvers extends Resolvers {
   Launch: ResolverMap;
+  Mutation: ResolverMap;
 }
 
-export const resolvers: AppResolvers = {
-  Launch: {
-    isInCart: (launch: LaunchTileTypes.LaunchTile, _, { cache }): boolean => {
-      const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({ 
-        query: GET_CART_ITEMS 
-      });
+export const resolvers = {
+  Mutation: {
+    addOrRemoveFromCart: (_, { id }: { id: string }, { cache }): string[] => {
+      const queryResult = cache
+        .readQuery<GetCartItemTypes.GetCartItems, any>({ 
+          query: GET_CART_ITEMS 
+        });
       if (queryResult) {
-        return queryResult.cartItems.includes(launch.id)
-      } 
-      return false;
-    }
+        const { cartItems } = queryResult;
+        const data = {
+          cartItems: cartItems.includes(id)
+            ? cartItems.filter((i) => i !== id)
+            : [...cartItems, id],
+        };
+        cache.writeQuery({ query: GET_CART_ITEMS, data });
+        return data.cartItems;
+      }
+      return [];
+    },
   },
 };
