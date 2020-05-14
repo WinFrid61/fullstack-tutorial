a2ead764b031a8eae23e68a2f874b806e4bf4d75
WinFrid61 <38008327+WinFrid61@users.noreply.github.com>
2020-05-14 21:54:30 +0300

    Создание GraphQL фрагментов

--- start/client/src/pages/launch.tsx
+++ start/client/src/pages/launch.tsx
@@ -2,10 +2,24 @@ import React, { Fragment } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag'; 
 
+import { LAUNCH_TILE_DATA } from './launches';
 import { Loading, Header, LaunchDetail } from '../components';
 import { ActionButton } from '../containers';
 import { RouteComponentProps } from '@reach/router';
 import * as LaunchDetailsTypes from './__generated__/LaunchDetails';
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      site
+      rocket {
+        type
+      }
+      ...LaunchTile
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
 interface LaunchProps extends RouteComponentProps {
   launchId?: any;
